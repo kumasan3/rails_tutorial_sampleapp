2139f7a4edbe991c66921ea32006db9da6dc3b15
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-23 22:01:40 +0900

    7.2.1 form_withを使用する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new  #属性情報だけを持った@userを作成。属性情報を元に、form_withがフォーム作成
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 end
