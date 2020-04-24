ba6bba4407712fa4f91eb13eaa83127dc5571cf0
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-24 14:31:25 +0900

    8.2 ログイン

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'Invalid email/password combination' # 本当は正しくない
+      render 'new'
+    end
   end
 
   def destroy
-    
+
   end
 end
