0409cccc0b1e191961105ac8ed7467735671a4bb
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-26 10:14:31 +0900

    11.2 アカウント有効化のメール送信

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { email.downcase! }
+  attr_accessor :remember_token, :activation_token
+  before_save :downcase_email
+  before_create :before_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -36,6 +37,16 @@ class User < ApplicationRecord
       SecureRandom.urlsafe_base64
     end
   end
+  private
+
+    def before_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+    def downcase_email
+      email.downcase!
+    end
 
 
 end
