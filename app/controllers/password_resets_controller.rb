eeb1f15f3995e525f70ef6f724dcbdf0a56ae3ff
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-27 14:05:31 +0900

    12.2 パスワード再設定のメール送信

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,19 @@
 class PasswordResetsController < ApplicationController
   def new
   end
+  def create # reset_tokenの作成
+    @user = User.find_by(email: params[:password_reset][:email])
+
+    if @user
+      @user.create_reset_digest #ダイジェストをDBに保存
+      @user.send_password_reset_email
+      flash[:info] = "Email sent with password reset instructions"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "address not found"
+      render "new"
+    end
+  end
 
   def edit
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
   before_create :before_activation_digest
   validates :name, presence: true, length: {maximum: 50}
@@ -33,6 +33,17 @@ class User < ApplicationRecord
 
   def activate
     update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    self.reset_digest = User.digest(reset_token)
+    update_attribute(:reset_digest, User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
 
   class << self
