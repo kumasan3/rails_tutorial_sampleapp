b5993ea47334af90c5f4aca034c222fdd0a125c9
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-27 19:40:09 +0900

    13.1.4 マイクロポストを改良する

---
+++ test/models/micropost_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class MicropostTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+
+  def setup
+    @user = users(:michael)
+    @micropost = @user.microposts.build(content: "testcontent")
+  end
+
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "should be invalid" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+
+  test "content should be present" do
+    @micropost.content  = "  "
+    assert_not @micropost.valid?
+  end
+
+  test "contetn should be less than 140 characters" do
+    @micropost.content =  "a" * 141
+    assert_not @micropost.valid?
+  end
+
+  test "contetn should be less than 140 characters #2" do
+    @micropost.content =  "a" * 140
+    assert @micropost.valid?
+  end
+
+
+end
