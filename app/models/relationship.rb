bed3aa0b71e87f64372e8702a89aa5f54b246862
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-29 15:40:54 +0900

    14.1.5 フォロワー

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,7 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
+  #外部キーが user_id 一つでは無いから、このように定義する必要あり。
   validates :follower_id, presence: true
   validates :followed_id, presence: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -80,5 +80,15 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "should follow and unfollow user" do
+    michael = users(:michael)
+    archer = users(:archer)
+    assert_not michael.following?(archer)
+    michael.follow(archer)
+    assert michael.following?(archer)
+    michael.unfollow(archer)
+    assert_not michael.following?(archer)
+  end
+
 end
 
