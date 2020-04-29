e9bfc12d637c0ee667b7f7ed3931bb0680d3cd19
kumasan3 <kumasan3san@yahoo.co.jp>
2020-04-29 22:22:32 +0900

    14.3.2 フィードを初めて実装する

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -18,6 +18,5 @@ class RelationshipsController < ApplicationController
       format.html { redirect_to @user }
       format.js
     end
-    
   end
 end
--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FollowingTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -24,6 +25,43 @@ class FollowingTest < ActionDispatch::IntegrationTest
       assert_select "a[href=?]", user_path(user), 3
     end
   end
+
+  test "should follow a user the standard way" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, params: {followed_id: @other.id } 
+    end
+    assert_redirected_to @other
+  end
+
+  test "should follow a user with Ajax" do
+    assert_difference "@user.following.count", 1 do
+      post relationships_path, params: {followed_id: @other.id }, xhr: true 
+    end
+  end
+
+  test "should unfollow an user the standard way" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(relationship)
+    end
+    assert_redirected_to @other
+  end
+
+  test "should unfollow an user with Ajax" do
+    @user.follow(@other)
+    relationship = @user.active_relationships.find_by(followed_id: @other.id)
+    get user_path(@other)
+    assert_select "div#follow_form>form>input.btn[value=?]", "Unfollow"
+    assert_difference "@user.following.count", -1 do
+      delete relationship_path(relationship), xhr: true
+    end
+    # get user_path(@other)
+    # assert_select "div#follow_form>form>input.btn[value=?]", "Follow"
+    
+  end
+
+
   
 
 
